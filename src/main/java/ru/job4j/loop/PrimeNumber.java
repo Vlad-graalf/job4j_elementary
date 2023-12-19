4c574f8f08b2c422b865b6b79d4ffe32fe4db350
Vlad <Vlad031999@yandex.by>
2023-12-19 13:34:01 +0300

    5.6. Простые числа [#156314]

---
+++ src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,21 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+    public static int calc(int fihish) {
+        int count = 0;
+        for (int number = 2; number <= fihish; number++) {
+            if (CheckPrimeNumber.check(number)) {
+               count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(PrimeNumber.calc(5));
+        System.out.println(PrimeNumber.calc(11));
+        System.out.println(PrimeNumber.calc(2));
+        System.out.println(PrimeNumber.calc(25));
+        System.out.println(PrimeNumber.calc(49));
+    }
+}
