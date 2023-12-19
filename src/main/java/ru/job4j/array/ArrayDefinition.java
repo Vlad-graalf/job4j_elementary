076a61487c34eee8939491d0bf4358375c092861
Vlad <Vlad031999@yandex.by>
2023-12-19 21:54:31 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println(surnames.length);
         float[] prices = new float[40];
         System.out.println(prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Vlad D.";
+        names[1] = "Ksenia B.";
+        names[2] = "Timofei D.";
+        names[3] = "Kirill B.";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
