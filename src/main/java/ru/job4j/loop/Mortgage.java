614877fc6a8272f5e095800e502e7c5a37643201
Vlad <Vlad031999@yandex.by>
2023-12-19 14:40:30 +0300

    5.7. Ипотека [#156315]

---
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(double amount, int salary, double percent) {
+        int year = 0;
+        while (amount > 0) {
+            amount = amount + (amount * percent) - salary;
+            year++;
+        }
+        return year;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Mortgage.year(1000, 1200, 0.01));
+        System.out.println(Mortgage.year(100, 120, 0.5));
+        System.out.println(Mortgage.year(1000, 1200, 0.2));
+    }
+}
